b317c892d52ba911c54f44614e835d0945355711
Pongchanan <pongchanansriwanna2547@gmail.com>
2025-02-17 16:31:02 +0700

    Add voting functionality and results display to polls application

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
-from django.template import loader
-from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 # Create your views here.
 def index(request):
@@ -13,6 +13,7 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    # print(question)
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
@@ -20,4 +21,19 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You are voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "You didn't select a choice.",
+        },)
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
